06120172a8bbb6c41407aac9b97f73bdef1a8f42
AdityaBhawsar <aditya.bhawsar@mutualmobile.com>
2021-09-15 13:16:36 +0530

    Added Dependency for Hilt Worker and set it in base app

--- app/src/main/java/com/example/pictureupload/BaseApp.kt
+++ app/src/main/java/com/example/pictureupload/BaseApp.kt
@@ -1,8 +1,19 @@
 package com.example.pictureupload
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class BaseApp: Application() {
+class BaseApp: Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
