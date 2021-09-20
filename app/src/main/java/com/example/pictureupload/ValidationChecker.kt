13525b64fd5bf635ca0fd2c5b51828ddcfdf63c5
AdityaBhawsar <aditya.bhawsar@mutualmobile.com>
2021-09-20 14:33:47 +0530

    Added a validation helper

---
+++ app/src/main/java/com/example/pictureupload/ValidationChecker.kt
@@ -0,0 +1,22 @@
+package com.example.pictureupload
+
+import com.example.pictureupload.usecases.AuthUseCase
+
+object ValidationChecker {
+
+    fun validateAuthParams(params: AuthUseCase.Params): ValidationResult {
+        if(params.user.isEmpty() && params.pass.isEmpty())
+            return ValidationResult.Failed("Please enter valid email and password")
+        if(params.user.isEmpty())
+            return ValidationResult.Failed("Please enter a valid email")
+        if(params.pass.isEmpty())
+            return ValidationResult.Failed("Please enter a valid password")
+
+        return ValidationResult.Success
+    }
+}
+
+sealed class ValidationResult{
+    object Success: ValidationResult()
+    class Failed(val msg: String): ValidationResult()
+}
