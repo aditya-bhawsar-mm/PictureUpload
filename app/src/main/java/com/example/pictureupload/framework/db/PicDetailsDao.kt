ae45333514430df7e4286f48485eb2e6b5a63523
AdityaBhawsar <aditya.bhawsar@mutualmobile.com>
2021-09-27 14:38:39 +0530

    Removed WildCard Import

--- app/src/main/java/com/example/pictureupload/framework/db/PicDetailsDao.kt
+++ app/src/main/java/com/example/pictureupload/framework/db/PicDetailsDao.kt
@@ -1,6 +1,10 @@
 package com.example.pictureupload.framework.db
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
 
 @Dao
 interface PicDetailsDao {
