5f9118ebe1ddd1dceccdf4295cc0d06e29a97a48
AdityaBhawsar <aditya.bhawsar@mutualmobile.com>
2021-09-27 11:13:45 +0530

    Added Explicit Return Types

--- app/src/main/java/com/example/pictureupload/framework/firebase/AuthProviderImpl.kt
+++ app/src/main/java/com/example/pictureupload/framework/firebase/AuthProviderImpl.kt
@@ -4,11 +4,12 @@ import com.example.pictureupload.data.AuthProvider
 import com.example.pictureupload.domain.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
 class AuthProviderImpl(private val auth : FirebaseAuth): AuthProvider {
 
-    override fun signInWithMailAndPassword(mail: String, password: String) = callbackFlow{
+    override fun signInWithMailAndPassword(mail: String, password: String): Flow<AuthResult> = callbackFlow{
 
         trySend(AuthResult.Loading)
 
--- data/src/main/java/com/example/pictureupload/data/AuthRepository.kt
+++ data/src/main/java/com/example/pictureupload/data/AuthRepository.kt
@@ -1,8 +1,11 @@
 package com.example.pictureupload.data
 
+import com.example.pictureupload.domain.AuthResult
+import kotlinx.coroutines.flow.Flow
+
 class AuthRepository (private val authProvider: AuthProvider){
 
-    fun signInWithPassword(mail:String, pass:String)
+    fun signInWithPassword(mail:String, pass:String): Flow<AuthResult>
         = authProvider.signInWithMailAndPassword(mail,pass)
 
 }
