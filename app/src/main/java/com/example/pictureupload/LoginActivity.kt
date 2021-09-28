fb4f6279043a7687272f28f56470ce42c863c272
AdityaBhawsar <aditya.bhawsar@mutualmobile.com>
2021-09-28 11:24:53 +0530

    Added Jetpack Compose

--- app/src/main/java/com/example/pictureupload/LoginActivity.kt
+++ app/src/main/java/com/example/pictureupload/LoginActivity.kt
@@ -1,13 +1,38 @@
 package com.example.pictureupload
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pictureupload.ui.theme.PictureUploadTheme
 
-@AndroidEntryPoint
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContent {
+            PictureUploadTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PictureUploadTheme {
+        Greeting("Android")
     }
 }
