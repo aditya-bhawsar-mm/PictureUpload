035ece285d32bb7ec546045038741cfcb0c34555
Shubham Singh <shubham.singh@mutualmobile.com>
2021-10-08 17:26:58 +0530

    Created and integrated blank composables into BottomNavigationBar.kt

--- app/src/main/java/com/example/pictureupload/ui/screens/landing_screen/components/BottomNavigationBar.kt
+++ app/src/main/java/com/example/pictureupload/ui/screens/landing_screen/components/BottomNavigationBar.kt
@@ -16,11 +16,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.pictureupload.ui.navigation.BottomNavigationHost
 import com.example.pictureupload.ui.screens.BottomNavItem
 
 @Composable
 fun BottomNavigationBar(modifier: Modifier = Modifier) {
-  // val navController = rememberNavController()
+  val navController = rememberNavController()
   val iconSize by remember { mutableStateOf(60.dp) }
   val navBarSize by remember { mutableStateOf(iconSize + 20.dp) }
   val screens = listOf(
@@ -35,33 +40,30 @@ fun BottomNavigationBar(modifier: Modifier = Modifier) {
       backgroundColor = MaterialTheme.colors.surface,
       modifier = modifier.height(navBarSize)
     ) {
-      // val navBackStackEntry by navController.currentBackStackEntryAsState()
-      // val currentDestination = navBackStackEntry?.destination
+      val navBackStackEntry by navController.currentBackStackEntryAsState()
+      val currentDestination = navBackStackEntry?.destination
 
       screens.forEach { screen ->
         BottomNavigationItem(
           icon = { Icon(painter = painterResource(id = screen.iconId), contentDescription = screen.route, modifier = modifier.height(iconSize)) },
           label = { Text(text = stringResource(id = screen.label), style = MaterialTheme.typography.body1) },
-          // selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-          // onClick = {
-          //   navController.navigate(screen.route) {
-          //     popUpTo(navController.graph.findStartDestination().id) {
-          //       saveState = true
-          //     }
-          //     launchSingleTop = true
-          //     restoreState = true
-          //   }
-          // },
-          //Remove these temporary 'onClick' and 'selected' parameters and uncomment the ones above after adding navBackStackEntry and currentDestination
-          onClick = {},
-          selected = false,
+          selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+          onClick = {
+            navController.navigate(screen.route) {
+              popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+              }
+              launchSingleTop = true
+              restoreState = true
+            }
+          },
           selectedContentColor = Color.White,
           unselectedContentColor = Color.LightGray,
           // alwaysShowLabel = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
         )
       }
     }
-  }) {
-    //todo: add the navHost component here
+  }) { innerPadding ->
+    BottomNavigationHost(navController = navController, innerPadding = innerPadding)
   }
 }
