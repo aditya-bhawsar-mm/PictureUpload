8f0d7dc18cb5acfdd55453446505764ba96f5201
AdityaBhawsar <aditya.bhawsar@mutualmobile.com>
2021-10-14 18:24:07 +0530

    Added KtLint Changes

--- app/src/main/java/com/example/pictureupload/framework/db/PicDetailsEntity.kt
+++ app/src/main/java/com/example/pictureupload/framework/db/PicDetailsEntity.kt
@@ -6,20 +6,20 @@ import com.example.pictureupload.domain.PicDetails
 
 // Entity Class for the table in the Database
 @Entity(tableName = "pic_tb")
-data class PicDetailsEntity (
+data class PicDetailsEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val path: String,
     val uploaded: Boolean
 )
 
-fun PicDetails.toEntity(): PicDetailsEntity{
+fun PicDetails.toEntity(): PicDetailsEntity {
     return PicDetailsEntity(
         this.id, this.path, this.uploaded
     )
 }
 
-fun PicDetailsEntity.toDetails(): PicDetails{
+fun PicDetailsEntity.toDetails(): PicDetails {
     return PicDetails(
         this.id, this.path, this.uploaded
     )
